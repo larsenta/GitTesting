e1aea06ab8b1c2c9799f20552efdd20be9888ef2
Thomas A. Larsen <larsentag@gmail.com>
2018-05-01 11:41:03 +0200

    Add label

--- GitTesting/ViewController.swift
+++ GitTesting/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelText.text = "My text"
+        print("View loaded")
     }
 
     override func didReceiveMemoryWarning() {

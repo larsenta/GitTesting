7f5bbbf3647335354c137183e2c3a5f616382e41
Thomas A. Larsen <larsentag@gmail.com>
2018-05-01 11:37:10 +0200

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by Thomas Andre Larsen on 1/5/18.
+//  Copyright © 2018 Thomas A. Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
